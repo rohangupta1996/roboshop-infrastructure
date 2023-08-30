5a7e2c9b8ae62085a5c6583c9d009ef5d26699f0
Rohan <rgrohangupta65@gmail.com>
2023-08-30 15:55:58 +0530

    Roboshop component

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,9 @@
-data "aws_caller_identity" "current" {}
+#data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "devops-practice-with-ansible"
-  owners      = [data.aws_caller_identity.current.account_id]
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
 
 resource "aws_instance" "ec2" {

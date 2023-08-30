341bb01437a6a1513e5e4ce1e5f1cbd2ff6c4002
Rohan <rgrohangupta65@gmail.com>
2023-08-30 18:31:02 +0530

    Roboshop component

--- ec2/main.tf
+++ ec2/main.tf
@@ -28,7 +28,7 @@ resource "null_resource" "provisioner" {
     inline = [
       "git clone https://github.com/rohangupta1996/robo-shell.git",
       "cd robo-shell",
-      "sudo bash frontend.sh"
+      "sudo bash ${var.component}.sh"
     ]
   }
 }

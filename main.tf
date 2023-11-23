0a1fc705703eea47e560daf1897ba3ba4cc095a9
Rohan <rgrohangupta65@gmail.com>
2023-11-23 18:10:34 +0530

    Roboshop component

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 module "vpc" {
-
   source = "git::https://github.com/rohangupta1996/tf-module-vpc.git"
-  env = var.env
-  tags = var.tags
-  
+  env    = var.env
+  tags   = var.tags
+
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
   public_subnets = each.value["public_subnets"]

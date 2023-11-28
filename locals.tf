d9cc675564272d60066a5b6c3dc71a3ba1cb0b6c
Rohan <rgrohangupta65@gmail.com>
2023-11-29 01:36:01 +0530

    Roboshop component

--- locals.tf
+++ locals.tf
@@ -3,4 +3,6 @@ locals {
   #private_subnet_ids = { for k, v in module.vpc["main"].private_subnet : k => tomap({"id" = v.id, "availability_zone" = v.availability_zone}) }
   #private_subnet_ids = [for k, v in module.vpc["main"].private_subnet : v.id]
   db_subnet_ids = tolist([module.vpc["main"].private_subnet["db-az1"].id, module.vpc["main"].private_subnet["db-az2"].id])
+  app_subnet_ids = tolist([module.vpc["main"].private_subnet["app-az1"].id, module.vpc["main"].private_subnet["app-az2"].id])
+  web_subnet_ids = tolist([module.vpc["main"].private_subnet["web-az1"].id, module.vpc["main"].private_subnet["web-az2"].id])
 }
--- main.tf
+++ main.tf
@@ -11,21 +11,21 @@ module "vpc" {
   private_subnet = each.value["private_subnet"]
 }
 
-#module "docdb" {
-#  source = "git::https://github.com/rohangupta1996/tf-module-docdb.git"
-#  env    = var.env
-#  tags   = var.tags
-#
-#
-#  for_each      = var.docdb
-#  engine      = each.value["engine"]
-#  backup_retention_period      = each.value["backup_retention_period"]
-#  preferred_backup_window      = each.value["preferred_backup_window"]
-#  skip_final_snapshot      = each.value["skip_final_snapshot"]
-#  engine_version      = each.value["engine_version"]
-#  subnet_ids = local.db_subnet_ids
-#
-#}
+module "docdb" {
+  source = "git::https://github.com/rohangupta1996/tf-module-docdb.git"
+  env    = var.env
+  tags   = var.tags
+
+
+  for_each      = var.docdb
+  engine      = each.value["engine"]
+  engine_version      = each.value["engine_version"]
+  backup_retention_period      = each.value["backup_retention_period"]
+  preferred_backup_window      = each.value["preferred_backup_window"]
+  skip_final_snapshot      = each.value["skip_final_snapshot"]
+  subnet_ids = local.db_subnet_ids
+
+}
 
 
 output "vpc" {
